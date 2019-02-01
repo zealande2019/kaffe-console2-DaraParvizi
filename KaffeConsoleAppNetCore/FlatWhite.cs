a4ca14284c69c6cd9a8c8360aa9388ccd355e74e
Dara Parvizi <dara0046@edu.easj.dk>
2019-02-01 13:24:23 +0100

    Ny klasse "flatwhite"

---
+++ KaffeConsoleAppNetCore/FlatWhite.cs
@@ -0,0 +1,22 @@
+﻿using kaffe;
+
+namespace KaffeConsoleAppNetCore
+{
+    public class FlatWhite : Kaffe , Imælk
+    {
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+    }
+}
